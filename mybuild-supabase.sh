651b060aa5f48cc9e22a384cacefaf4aeeba7296
Damien Berezenko <qdrddr@gmail.com>
2025-02-12 13:25:14 -0600

    Update index.yaml & build with supabase-kubernetes for helm version 0.1.3

--- mybuild-supabase.sh
+++ mybuild-supabase.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+GITREPO_NAME=supabase-kubernetes
+GITREPO_CHART_PATH=charts/supabase
+HELM_CHART_NAME=supabase
+
+####################
 MYHELM_REPO_PATH=$PWD
 mkdir -p ${MYHELM_REPO_PATH}/build/
-
-GITREPO_NAME=supabase-kubernetes
 #cd .. ; git clone https://github.com/supabase-community/supabase-kubernetes.git
 cd ../${GITREPO_NAME}
 git pull
@@ -11,10 +14,10 @@ git pull
 #echo "GITREPO_NAME: ${GITREPO_NAME} | REPO_VERSION: ${REPO_VERSION}"
 #git checkout ${REPO_VERSION}
 
-HELM_VERSION=$(grep '^version:' ./charts/supabase/Chart.yaml | awk '{print $2}')
+HELM_VERSION=$(grep '^version:' ./${GITREPO_CHART_PATH}/Chart.yaml | awk '{print $2}')
 echo "HELM_VERSION: ${HELM_VERSION}"
 
-helm package ./charts/supabase -d build/
+helm package ./${GITREPO_CHART_PATH} -d build/
 helm repo index ./
 # sed 's+build+head+g' ./index.yaml > ./index.yaml
 
@@ -37,8 +40,15 @@ cp index.yaml ${MYHELM_REPO_PATH}/index-${GITREPO_NAME}.yaml
 
 cd ${MYHELM_REPO_PATH}
 cp index.yaml index-previous.yaml
+#Merge index.yaml files
 yq eval-all '. as $item ireduce ({}; . *+ $item)' "index-previous.yaml" "index-${GITREPO_NAME}.yaml" > index.yaml
 
 git add *
+git status
 git commit -m "Update index.yaml & build with ${GITREPO_NAME} for helm version ${HELM_VERSION}"
 git push
+
+#verify
+helm repo add qdrddr https://raw.githubusercontent.com/qdrddr/helm/refs/heads/main
+helm repo update qdrddr
+helm search repo qdrddr/${HELM_CHART_NAME}
